b469e6e949acf2a746904b65faa4f03e6f2915c0
Sahil Barua <sahilrik@gmail.com>
2022-03-15 23:41:00 +0530

    task list update

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,18 +1,30 @@
 import 'package:flutter/material.dart';
 import 'task_tile.dart';
+import 'package:todoeye/models/task.dart';
 
-class TaskList extends StatelessWidget {
+class TaskList extends StatefulWidget {
   const TaskList({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<TaskList> createState() => _TaskListState();
+}
+
+class _TaskListState extends State<TaskList> {
+  List<Task> tasks = [
+    Task(name: 'buy milk'),
+    Task(name: 'buy apple'),
+    Task(name: 'buy milk'),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: const <Widget>[
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(isChecked: false, taskTile: tasks[index].name);
+      },
+      itemCount: tasks.length,
     );
   }
 }
